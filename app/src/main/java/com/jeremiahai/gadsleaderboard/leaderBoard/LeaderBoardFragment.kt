cb2ae476b41d14bd2e100ff65f0c6c05d2923c3a
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2020-09-10 21:57:21 +0100

    Observe data in fragments

--- app/src/main/java/com/jeremiahai/gadsleaderboard/leaderBoard/LeaderBoardFragment.kt
+++ app/src/main/java/com/jeremiahai/gadsleaderboard/leaderBoard/LeaderBoardFragment.kt
@@ -5,19 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.jeremiahai.gadsleaderboard.R
+import com.jeremiahai.gadsleaderboard.data.model.GadsLearner
+import com.jeremiahai.gadsleaderboard.utils.POSITION
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LeaderBoardFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var v: View? = null
+    val viewModel: LeaderBoardViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leader_board, container, false)
+        v = inflater.inflate(R.layout.fragment_leader_board, container, false)
+        arguments?.getInt(POSITION)?.let { setupViewModel(it) }
+        return v;
+    }
+
+    private fun setupViewModel(position: Int) {
+        val observer = Observer<List<GadsLearner>> {
+
+        }
+
+        viewModel.apply {
+            if (position == 0) learningLeadersLiveData.observe(viewLifecycleOwner, observer)
+            else skillIqLeadersLiveData.observe(viewLifecycleOwner, observer)
+        }
     }
 
 }
