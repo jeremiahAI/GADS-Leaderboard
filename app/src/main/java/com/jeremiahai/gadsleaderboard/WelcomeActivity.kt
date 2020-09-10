a41830d8f77cfa1f8d618cd4eb8fece7ac4556f6
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2020-09-10 19:05:52 +0100

    Initial commit

---
+++ app/src/main/java/com/jeremiahai/gadsleaderboard/WelcomeActivity.kt
@@ -0,0 +1,27 @@
+package com.jeremiahai.gadsleaderboard
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+        Handler().postDelayed({ navigateToMain() }, 5000)
+    }
+
+    private fun navigateToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivityWithFadeAnimation(intent)
+    }
+
+}
+
+fun Activity.startActivityWithFadeAnimation(intent: Intent) {
+    startActivity(intent)
+    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+}
