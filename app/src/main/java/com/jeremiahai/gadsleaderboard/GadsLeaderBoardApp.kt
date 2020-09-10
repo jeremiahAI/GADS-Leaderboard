016978ad6c1126826f096d880504c3fd7ff93c31
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2020-09-10 21:17:17 +0100

    Add dependency injection with Remote module

---
+++ app/src/main/java/com/jeremiahai/gadsleaderboard/GadsLeaderBoardApp.kt
@@ -0,0 +1,8 @@
+package com.jeremiahai.gadsleaderboard
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class GadsLeaderBoardApp : Application()
---
+++ app/src/main/java/com/jeremiahai/gadsleaderboard/dependencyInjection/RemoteModule.kt
@@ -0,0 +1,64 @@
+package com.jeremiahai.gadsleaderboard.dependencyInjection
+
+import com.google.gson.Gson
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.jeremiahai.gadsleaderboard.BuildConfig
+import com.jeremiahai.gadsleaderboard.data.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class RemoteModule {
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    private val GADS_BASE_URL = "https://gadsapi.herokuapp.com"
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return Gson()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(1, TimeUnit.MINUTES)
+            .writeTimeout(1, TimeUnit.MINUTES)
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .apply {
+                if (!BuildConfig.BUILD_TYPE.contains("release")) {
+                    val logging = HttpLoggingInterceptor()
+                    logging.level = HttpLoggingInterceptor.Level.BODY
+                    addNetworkInterceptor(logging)
+//                        .addInterceptor(OkHttpProfilerInterceptor())
+                }
+            }
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(GADS_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+    }
+}
